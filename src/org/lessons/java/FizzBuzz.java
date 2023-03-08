f7422cd8abfb67a2848b6ab4fce30ff58a6fede2
Alessandro-Sebastiano <alesebastiano15@gmail.com>
2023-03-08 16:05:23 +0100

    fizz-buzz-class

---
+++ src/org/lessons/java/FizzBuzz.java
@@ -0,0 +1,24 @@
+package org.lessons.java;
+
+public class FizzBuzz {
+
+    public static void  main(String[] arg){
+
+        for (int i = 1; i <= 100; i++) {
+
+            if (i % 3 != 0 && i % 5 != 0) {
+                System.out.println(i);
+            }
+
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            }
+        }
+
+    }
+
+}
