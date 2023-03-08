4034069f3ae7c4925e496722c0f151ce90f2d9a1
Alessandro-Sebastiano <alesebastiano15@gmail.com>
2023-03-08 15:06:03 +0100

    train-ticket-main-method

--- src/org/lessons/java/TrainTicket.java
+++ src/org/lessons/java/TrainTicket.java
@@ -1,4 +1,38 @@
 package org.lessons.java;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class TrainTicket {
+
+    public static void main(String[] arg){
+
+        int distanceKm;
+        int age;
+        double kmPrice = 0.21;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Inserisci la distanza da percorrere in KM: ");
+        distanceKm = Integer.parseInt(input.nextLine());
+        System.out.print("Inserisci la tua eta' : ");
+        age = Integer.parseInt(input.nextLine());
+
+        double ticketPrice = distanceKm * kmPrice;
+
+        if(age < 18){
+            ticketPrice -= (ticketPrice * 20) / 100;
+            System.out.println("Hai diritto al 20% di sconto");
+        } else if (age > 65) {
+            ticketPrice -= (ticketPrice * 40) / 100;
+            System.out.println("Hai diritto al 40% di sconto");
+        }else{
+            System.out.println("Non hai diritto allo sconto");
+        }
+
+        System.out.println("Distanza totale: " + distanceKm + " Km");
+        System.out.println("Eta passegero: " + age);
+        System.out.println("Il costo del biglietto e: $" + ticketPrice);
+
+    }
+
 }
